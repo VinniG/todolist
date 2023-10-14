5f2debb5afe482de3f409b2ac338ba5b09713411
VinniG <VinniG@noreply.github.com>
2023-10-14 14:43:22 -0300

    Comments

--- src/main/java/br/com/viniciusgomes/todolist/user/UserController.java
+++ src/main/java/br/com/viniciusgomes/todolist/user/UserController.java
@@ -10,29 +10,39 @@ import org.springframework.web.bind.annotation.RestController;
 
 import at.favre.lib.crypto.bcrypt.BCrypt;
 
-// Anotations SpringBoot -> Estudar conceito
-
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
     @Autowired
     private IUserRepository userRepository;
-    
+
+    // O controlador é uma classe que lida com as solicitações relacionadas a usuários.
+
     @PostMapping("/")
     public ResponseEntity create(@RequestBody UserModel userModel) {
-        var user = this.userRepository.findByUsername(userModel.getUsername());
+        // Este método é mapeado para tratamento de solicitações POST na rota "/users".
 
-        if(user != null) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+        // Procura um usuário no repositório com base no nome de usuário fornecido no modelo.
+
+        if (user != null) {
+            // Se um usuário com o mesmo nome de usuário já existe, retorna uma resposta de erro.
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
         }
 
         var passwordHashred = BCrypt.withDefaults()
             .hashToString(12, userModel.getPassword().toCharArray());
+        // Usa o algoritmo BCrypt para criar um hash da senha fornecida no modelo.
 
         userModel.setPassword(passwordHashred);
+        // Define a senha no modelo como o hash recém-gerado.
 
         var userCreated = this.userRepository.save(userModel);
+        // Salva o novo usuário no repositório.
+
         return ResponseEntity.status(HttpStatus.OK).body(userCreated);
+        // Retorna uma resposta de sucesso com o usuário criado.
     }
 }
+
